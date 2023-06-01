147d97a5f7fb04433016a5265356f7c9e6cde55b
git-pardeep <pardeepkambojj@gmail.com>
2023-06-01 08:40:41 +0530

    second

--- src/main/java/com/base/BaseClass.java
+++ src/main/java/com/base/BaseClass.java
@@ -5,18 +5,32 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseClass {
 public static WebDriver driver;
 	public void launchBrowser(){
 		System.out.println("hello ");
+		if (System.getProperty("Browser").equalsIgnoreCase("chrome")) {
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
-		
+		}
+		else if (System.getProperty("Browser").equalsIgnoreCase("chrome")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver=new FirefoxDriver();
+		}else if (System.getProperty("Browser").equalsIgnoreCase("chrome")) {
+			WebDriverManager.edgedriver().setup();
+			driver =new EdgeDriver();
+		}
+		else {
+			System.out.println("enter valid browser name");
+		}
 	}
 	public void launchURL() throws InterruptedException {
-		driver.get("https://www.google.com/");
+		driver.get(System.getProperty("URL"));
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
 		WebElement search = driver.findElement(By.id("APjFqb"));
